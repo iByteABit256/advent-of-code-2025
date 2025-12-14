5bce5e72592d3065f92b20d7a84796bf3ed5f800
Pavlos Smith <paulsmith4561@gmail.com>
2025-12-14 23:22:02 +0200

    Day 8 Part 1 completed, to be improved

---
+++ src/bin/08.rs
@@ -0,0 +1,188 @@
+use std::collections::HashSet;
+
+advent_of_code::solution!(8);
+
+const INF: u64 = u64::MAX;
+
+fn dist(p: &[u64], q: &[u64]) -> u64 {
+    if p.len() != q.len() {
+        panic!("invalid input, all points need to have the same dimensions");
+    }
+
+    // Squared Eucledean distance
+    (0..p.len())
+        .map(|i| p[i].abs_diff(q[i]))
+        .map(|d| d * d)
+        .sum()
+}
+
+fn distance_matrix(points: &[Vec<u64>]) -> Vec<Vec<u64>> {
+    let n = points.len();
+    let mut m = vec![vec![0; n]; n];
+
+    for i in 0..n {
+        for j in 0..n {
+            if m[i][j] != 0 {
+                continue;
+            }
+
+            if i == j {
+                m[i][j] = 0;
+            }
+
+            m[i][j] = dist(&points[i], &points[j]);
+            m[j][i] = m[i][j];
+        }
+    }
+
+    m
+}
+
+fn minimum_distance(m: &[Vec<u64>], history: &HashSet<(usize, usize)>) -> (usize, usize) {
+    let mut min = INF;
+    let mut pair = (0, 0);
+
+    for (i, row) in m.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            if i == j {
+                continue;
+            }
+
+            if history.contains(&(i, j)) || history.contains(&(j, i)) {
+                /*
+                println!(
+                    "minimum_distance: skipped {},{} because it existed in history",
+                    i, j
+                );
+                */
+                continue;
+            }
+
+            if *col < min {
+                min = *col;
+                pair = (i, j);
+            }
+        }
+    }
+
+    pair
+}
+
+type Circuit = HashSet<usize>;
+
+fn print_matrix(m: &[Vec<u64>]) {
+    println!("Distance matrix:\n");
+    m.iter().for_each(|row| {
+        row.iter().for_each(|col| print!("{} ", col));
+        println!();
+    });
+}
+
+fn to_string(c: &Circuit) -> String {
+    let mut s = "{".to_owned();
+    c.iter().for_each(|p| s.push_str(&(p.to_string() + ",")));
+    s.push('}');
+    s
+}
+
+fn print_all(circuits: &[Circuit]) {
+    circuits.iter().for_each(|c| println!("{}", to_string(c)));
+}
+
+fn connect(c1: &Circuit, c2: &Circuit) -> Circuit {
+    c1.union(c2).map(|p| p.to_owned()).collect()
+}
+
+fn calculate_circuits(m: &[Vec<u64>], num: u64) -> u64 {
+    //print_matrix(m);
+
+    let n = m.len();
+    let mut circuits: Vec<Circuit> = (0..n).map(|i| HashSet::from([i])).collect();
+    let mut min_history = HashSet::new();
+
+    for k in 0..num {
+        //println!("Circuits after {} iterations:", k);
+        //print_all(&circuits);
+
+        let (i, j) = minimum_distance(m, &min_history);
+        min_history.insert((i, j));
+        //println!("Current minimum distance: {},{}", i, j);
+
+        let l = circuits
+            .iter()
+            .enumerate()
+            .filter(|(_, c)| c.contains(&i))
+            .map(|(idx, _)| idx)
+            .next()
+            .unwrap_or_else(|| panic!("no circuit found for point {}", i));
+        let r = circuits
+            .iter()
+            .enumerate()
+            .filter(|(_, c)| c.contains(&j))
+            .map(|(idx, _)| idx)
+            .next()
+            .unwrap_or_else(|| panic!("no circuit found for point {}", j));
+
+        if l == r {
+            continue;
+        }
+
+        let c_new = connect(&circuits[l], &circuits[r]);
+        //println!("New circuit: {}", to_string(&c_new));
+
+        if l < r {
+            circuits.remove(r);
+            circuits.remove(l);
+        } else {
+            circuits.remove(l);
+            circuits.remove(r);
+        }
+        circuits.push(c_new);
+    }
+
+    circuits.sort_by_key(|c| c.len());
+    println!("Final circuits:");
+    print_all(&circuits);
+    circuits
+        .iter()
+        .rev()
+        .take(3)
+        .map(|c| c.len() as u64)
+        .product()
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let points: Vec<Vec<u64>> = input
+        .lines()
+        .map(|l| {
+            l.split(",")
+                .map(|c| c.parse::<u64>().expect("not a number"))
+                .collect()
+        })
+        .collect();
+
+    let m = distance_matrix(&points);
+
+    Some(calculate_circuits(&m, 1000))
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(40));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
